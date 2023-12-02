2bb42f2827b1dd69598af6913ae6238dfa78850b
AzbijariKeyan <kiwi.azbijari@gmail.com>
2023-12-02 14:55:37 -0800

    US7-1, 7-2, 7-3
    #128
    #129
    #130
    just need to make auth go through server and this will work just fine

---
+++ src/main/java/controller/ErrorController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ErrorController {
+
+    @FXML
+    private Text errorMessage;
+
+    public void setErrorMessage(String message) {
+        errorMessage.setText(message);
+    }
+
+    @FXML
+    private void handleClose() {
+        Stage stage = (Stage) errorMessage.getScene().getWindow();
+        stage.close();
+    }
+}
