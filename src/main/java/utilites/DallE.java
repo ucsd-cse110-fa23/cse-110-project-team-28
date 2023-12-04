0e5d82e8165cb14e9ef1c1933a2fa0eec14986b0
alanm319 <alanm319@gmail.com>
2023-12-04 14:00:25 -0800

    added dalle image generation utility

---
+++ src/main/java/utilites/DallE.java
@@ -0,0 +1,79 @@
+package utilites;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import utilites.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DallE {
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/images/generations";
+    private static final String API_KEY = "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";
+    private static final String MODEL = "dall-e-3";
+    private static final String imagePromptTemplate = "[recipeName], shallow-focus, 35mm, Canon EOS 5D Mark IV DSLR, f/5.6 aperture, 1/125 second shutter speed, ISO 100 --ar 2:3 --q 2 --v 4";
+
+    public static String generateImageURL(String recipeName) throws IOException, InterruptedException, URISyntaxException {
+        
+        //Set request parameters
+        int n = 1;
+
+        //Create a request body which you will pass into request object
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("model", MODEL);
+        requestBody.put("prompt", imagePromptTemplate.replace("[recipeName]", recipeName));
+        requestBody.put("n", n);
+        requestBody.put("size", "1024x1024");
+
+        //create HTTP client
+        HttpClient client = HttpClient.newHttpClient();
+
+        //create the request object
+        HttpRequest request = HttpRequest
+            .newBuilder()
+            .uri(URI.create(API_ENDPOINT))
+            .header("Content-Type", "application/json")
+            .header("Authorization", String.format("Bearer %s", API_KEY))
+            .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+            .build();
+
+        //Send the request and receive the response
+        HttpResponse<String> response = client.send(
+            request,
+            HttpResponse.BodyHandlers.ofString()
+        );
+
+        //Process the response
+        String responseBody = response.body();
+
+        JSONObject responseJSON = new JSONObject(responseBody);
+
+        //System.out.println(responseBody);
+
+        //Processing the response
+        JSONArray data = responseJSON.getJSONArray("data");
+        String generatedImageURL = data.getJSONObject(0).getString("url");
+
+        Logger.log("DALL-E Response: ");
+        Logger.log(generatedImageURL);
+
+        return generatedImageURL;
+
+        
+        // try(
+        //     InputStream in = new URI(generatedImageURL).toURL().openStream()
+        //     ){
+        //     Files.copy(in, Paths.get("image.jpg"));
+        // }
+
+        
+    }
+}
