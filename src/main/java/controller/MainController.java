468243cdb6f71477a1b2d5eac1fa69112549878d
Raymond Wang <me@waymondrang.com>
2023-12-04 12:24:52 -0800

    ui and logoutbuttonhandler

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -133,7 +133,7 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    private void logoutHandler() throws IOException {
+    private void logoutButtonHandler() throws IOException {
         clearStoredCredentials();
         SceneHelper.switchToAuthenticationScene();
     }
